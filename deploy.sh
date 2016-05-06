c9e67cf6c4c572f35274a4b0650f927c7abbf2f5
Adnan <adnan@websemantics.ca>
2016-05-06 14:46:29 -0400

    Trying another version of nodejs

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,11 @@ BASEDIR=$(cd $(dirname $0) && pwd)
 # install npm and run gulp
 npm install
 
-# Create a new Git repo in dist folder
+# Create a new Git repo in dist folder, set user details, and issue first commit & force push
 cd "$BASEDIR/dist"
 git init
-
-# Set user details, and issue first commit, force push
 git config user.name "iAyeBot"
 git config user.email "iayebot@websemantics.ca"
 git add .
-git commit -m "Deploy to gh-pages"
+git commit -m "Publish to gh-pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
